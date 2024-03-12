b674cbc7006a20fd56351674c94daa83d3b39744
onederlnd <46465472+onederlnd@users.noreply.github.com>
2024-03-12 11:58:28 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
-import Services from './components/Services';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 
 const App = () => {
   return (
     <div className='bg-blue-50 min-h-[100vh] min-w-[100vh]'>
-      <Header />
-      <Main />
-      <Services />
-      <Contact />
-      <Footer />
+test
     </div>
   )
 }
